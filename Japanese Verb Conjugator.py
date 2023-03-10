976a9d256ae152956312a1985bf8decd00b12ae9
Dennis Estillero <dmestillero@gmail.com>
2023-03-10 19:22:50 +0000

    Created ichidan_verb and godan_veb classes

--- Japanese Verb Conjugator.py
+++ Japanese Verb Conjugator.py
@@ -32,52 +32,67 @@ alphabet_by_vowel = [
     ["ん"]
 ]
 
+class ichidan_verb:
+    def __init__(self, verb):
+        self.verb = verb
+    
+    def polite(verb):
+            verb_stem = verb[:-1]
+            print(f"The Present Polite Form of this verb is: {verb_stem}ます")
+    
+    def polite_negative(verb):
+            verb_stem = verb[:-1]
+            print(f"The Present Polite Negative Form of this verb is: {verb_stem}ません")
+
+class godan_verb:
+    def __init__(self, verb):
+        self.verb = verb
+        
+    def polite(verb):
+            verb_stem = verb[:-1]
+            for i in range(len(alphabet_by_first_letter)):
+                if verb[-1] in alphabet_by_first_letter[i]:
+                    row = i
+            prefix_vowel = alphabet_by_first_letter[row][1]
+            print(f"{verb_stem}{prefix_vowel}ます")
+                
+    def polite_negative(verb):
+            verb_stem = verb[:-1]
+            for i in range(len(alphabet_by_first_letter)):
+                if verb[-1] in alphabet_by_first_letter[i]:
+                    row = i
+            prefix_vowel = alphabet_by_first_letter[row][1]
+            print(f"{verb_stem}{prefix_vowel}ません")
+
+
+
 verb = input("Please enter the verb in dictionary form: ")
 
 # Dictionary Form to Polite Form
 
 # Irregular Verbs 
 if verb == "する":
-    print(f"{alphabet_by_vowel[1][2]}ます")
+    print(f"します")
 
 elif verb == "くる":
     print("きます")
 
-# Ichidan Verbs
 elif verb[-1] == "る" and verb[-2] in alphabet_by_vowel[1] or verb[-2] in alphabet_by_vowel[3]:
-    verb_stem = verb[:-1]
-    print(f"The Present Polite Form of this verb is: {verb_stem}ます")
-    
-# Godan Verbs
-elif verb[-1] in alphabet_by_vowel[2] and verb[-2] in alphabet_by_vowel[0] or alphabet_by_vowel[2] or alphabet_by_vowel[4]:
-    verb_stem = verb[:-1]
-    for i in range(len(alphabet_by_first_letter)):
-        if verb[-1] in alphabet_by_first_letter[i]:
-            row = i
-    prefix_vowel = alphabet_by_first_letter[row][1]
-    print(f"{verb_stem}{prefix_vowel}ます")
-    
+    ichidan_verb.polite(verb)
 
-# Dictionary Form to Polite Negative Form
+elif verb[-1] in alphabet_by_vowel[2] and verb[-2] in alphabet_by_vowel[0] or alphabet_by_vowel[2] or alphabet_by_vowel[4]:
+    godan_verb.polite(verb)
+
 
 # Irregular Verbs 
 if verb == "する":
-    print(f"{alphabet_by_vowel[1][2]}ません")
+    print(f"しません")
 
 elif verb == "くる":
     print("きません")
 
+elif verb[-1] == "る" and verb[-2] in alphabet_by_vowel[1] or verb[-2] in alphabet_by_vowel[3]:
+    godan_verb.polite_negative(verb)
 
-# Ichidan Verbs
-if verb[-1] == "る" and verb[-2] in alphabet_by_vowel[1] or verb[-2] in alphabet_by_vowel[3]:
-    verb_stem = verb[:-1]
-    print(f"The Present Polite Negative Form of this verb is: {verb_stem}ません")
-
-# Godan Verbs
 elif verb[-1] in alphabet_by_vowel[2] and verb[-2] in alphabet_by_vowel[0] or alphabet_by_vowel[2] or alphabet_by_vowel[4]:
-    verb_stem = verb[:-1]
-    for i in range(len(alphabet_by_first_letter)):
-        if verb[-1] in alphabet_by_first_letter[i]:
-            row = i
-    prefix_vowel = alphabet_by_first_letter[row][1]
-    print(f"{verb_stem}{prefix_vowel}ません")
+    godan_verb.polite_negative(verb)
